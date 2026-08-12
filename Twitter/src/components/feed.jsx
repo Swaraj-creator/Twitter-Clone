816d107f1159beb35821a74c0feba21098cb9fd2
Swaraj-creator <swarajkumar7011@gmail.com>
2026-08-12 17:05:19 +0530

    Completed styling create post box

--- Twitter/src/components/feed.jsx
+++ Twitter/src/components/feed.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
-import { TbSettings2 } from "react-icons/tb";
+import { TbSettings2, TbGif } from "react-icons/tb";
+import { RxImage } from "react-icons/rx";
+import { MdOutlineGifBox, MdOutlineEmojiEmotions, MdSchedule } from "react-icons/md";
+import { BiPoll } from "react-icons/bi";
+import { SlLocationPin } from "react-icons/sl";
 
 const Feed = () => {
   return (
@@ -12,7 +16,32 @@ const Feed = () => {
                 </div>
                 <div className="settingsBtn"><TbSettings2 /></div>
                 <div className="createPostArea">
-                    
+                    <div className="profilePic">
+                        <img src="https://i.pinimg.com/736x/2b/da/a5/2bdaa5be4e74c169603f18cec36f594c.jpg" alt="" />
+                    </div>
+                    <div className="inputArea">
+                        <textarea name="postContent" id="postContent" onInput={(e) => {
+                            e.target.style.height = "auto";
+                            e.target.style.height = `${e.target.scrollHeight}px`;
+                            if(document.getElementById("postContent").value.trim() != "" && document.getElementById("postContent").value.trim() != " ") {
+                                document.querySelector(".placeHolder").style.display = "none";
+                            } else {
+                                document.querySelector(".placeHolder").style.display = "block";
+                            }
+                        }}></textarea>
+                        <p className="placeHolder">What is Happening?</p>
+                        <div className="optionsPanel">
+                            <div className="options">
+                                <span><RxImage /></span>
+                                <span><TbGif /></span>
+                                <span><BiPoll></BiPoll></span>
+                                <span><MdOutlineEmojiEmotions></MdOutlineEmojiEmotions></span>
+                                <span><MdSchedule></MdSchedule></span>
+                                <span><SlLocationPin></SlLocationPin></span>
+                            </div>
+                            <button onClick={() => {}}>Post</button>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className="feed">
