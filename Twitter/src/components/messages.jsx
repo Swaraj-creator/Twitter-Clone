73a50beee8ad6f84dd1e52d01477f80c1fc31942
Swaraj-creator <swarajkumar7011@gmail.com>
2026-09-08 01:34:59 +0530

    created messages layout

--- Twitter/src/components/messages.jsx
+++ Twitter/src/components/messages.jsx
@@ -4,7 +4,28 @@ import '../messagesBox.css'
 const Messages = () => {
   return (
     <div className="messagesBox">
-      
+      <div className="chatsList">
+        <h3>Your Chats</h3>
+        <input type="text" placeholder='Search for Chat...'/>
+        <div className="allChats">
+
+        </div>
+      </div>
+      <div className="chatsBox">
+        <div className="chatRoomInfo">
+          <button className="backBtn"></button>
+          <div className="chatRoomPfp"></div>
+          <p className="chatRoomName">Swaraj Kashyap</p>
+          <p className="chatRoomHandle">@swarajkashyap</p>
+        </div>
+        <div className="allMessages">
+          <div className="message">
+            <div className="text-content">
+              <p>Heyyyyyyy! I am swaraj.</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
